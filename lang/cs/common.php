c2ca7a955a6c849f203ece63d94b6716e54bab53
PavelZanek <zanek.pavel@gmail.com>
2024-12-31 13:00:51 +0100

    Added language switcher

---
+++ lang/cs/common.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'locales' => [
+        'cs' => 'Čeština',
+        'en' => 'Angličtina',
+    ],
+    'flags' => [
+        'cs' => 'https://cdn.jsdelivr.net/gh/lipis/flag-icon-css@master/flags/4x3/cz.svg',
+        'en' => 'https://cdn.jsdelivr.net/gh/lipis/flag-icon-css@master/flags/4x3/gb.svg',
+    ],
+];
