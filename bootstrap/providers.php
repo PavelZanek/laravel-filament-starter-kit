f6964f9303b8155af012f49a6f0295e514c85a8c
PavelZanek <zanek.pavel@gmail.com>
2025-01-02 23:02:43 +0100

    Laravel Telescope init

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\Filament\AuthPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
